4380425fb4e4d295c201da065c484a4bee7f4060
mbenjak <marko.benjak@dhimahi.com>
2024-01-06 11:24:04 +0100

    Update alternative

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import '../node_modules/bootstrap/js/index.esm'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
