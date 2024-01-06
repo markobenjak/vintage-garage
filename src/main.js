3b4de9a3b2ee7321e6a1923888ab45781e139032
mbenjak <marko.benjak@dhimahi.com>
2024-01-06 11:53:28 +0100

    Update alternative

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '../node_modules/bootstrap/js/index.esm'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
-import { createApp } from 'vue'
+import { createApp } from '../node_modules/vue/dist/vue.esm-browser.prod'
 import App from './App.vue'
 
 createApp(App).mount('#app')
