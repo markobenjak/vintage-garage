ba2c69abfc39098330285d9ac9ca02ff17c934ab
mbenjak <marko.benjak@dhimahi.com>
2024-01-06 12:09:24 +0100

    Update alternative

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'vite/modulepreload-polyfill'
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: './src/main.js',
+    },
+  },
   plugins: [
     vue(),
   ],
